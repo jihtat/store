82cccf1cb54c6e01c637072fe093a6ae1077836d
fatiha <tihafat78@gmail.com>
2023-02-02 16:11:47 +0000

    imported a BrowserRouter , Routes and Route from react rouder dom , and used them for routing pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 import { Home } from './components/Home';
+import {BrowserRouter} from 'react-router-dom'  
+import {Shop} from './components/Shop';
+import {About} from './components/About';
+import {Contact} from './components/Contact';
+import {Routes, Route} from 'react-router-dom'
 import './App.css';
 
 function App() {
  
   return (
-    <div>
+      <BrowserRouter>
      <Navbar/>
-     <Home/>
+     <Routes>
+     <Route path='/' element={<Home/>}></Route>
+      <Route path='Shop' element={<Shop/>}></Route>
+      <Route path='About' element={<About/>}></Route>
+      <Route path='Contact' element={<Contact/>}></Route>
+     </Routes>
      <Footer/>
-    </div>
+    </BrowserRouter>
+   
   );
+  
 }
 
 export default App;
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,7 @@
 import { logDOM } from '@testing-library/react'
 import '../style.css'
 import React, { useState, useEffect } from 'react';
+import {NavLink} from 'react-router-dom';
 import logo from '../cropped.png';
 import { FaShoppingBag } from 'react-icons/fa'
 import { FaSortDown } from 'react-icons/fa'
@@ -15,12 +16,12 @@ export function Navbar() {
       <img src={logo} height="60px" width="90px" className="logo" alt="logo" /></a>
     <div>
       <ul id="navbar">
-        <li ><a className="active" href="">Home</a></li>
-        <li><a href="">Shop</a></li>
-        <li><a href="">About</a></li>
-        <li><a href="">Contact</a></li>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/Shop">Shop</NavLink></li>
+        <li><NavLink to="/About">About</NavLink></li>
+        <li><NavLink to="/Contact">Contact</NavLink></li>
        <Category/>
-        <li><a><FaShoppingBag /></a></li>
+        <li><NavLink to="/Contact"><FaShoppingBag /></NavLink></li>
       </ul>
     </div>
   </section>
